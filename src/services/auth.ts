4501cf5bec8c6d0c30fed75aa5da049c5fbbd457
Denis Zheleznov <zhel1@yandex.ru>
2024-02-11 04:17:24 -0800

    Handle error "User already in system"

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,6 +2,8 @@ import {TUser, SignInInput} from "../models/TUser.ts";
 import AuthApi from "../api/auth.ts";
 import {responseHasError} from "../utils/api.utils.ts";
 import {getChats} from "./chats";
+import {RoutesStrs} from "../core/config";
+import Router from "../core/router";
 
 const authApi = new AuthApi();
 
@@ -17,6 +19,10 @@ const signup = async (data: TUser) => {
 const signin = async (data: SignInInput) => {
     const response = await authApi.signin(data);
     if (responseHasError(response)) {
+        if (response.data.reason === "User already in system") {
+            Router.getRouter().go(RoutesStrs.messenger);
+        }
+
         throw Error(response.data.reason)
     }
     await getUser();
